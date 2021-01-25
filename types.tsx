657f229bc75c39a9ad38a5ab14a973ae8018d6a9
Leonardo Badilla <leopardolalia@gmail.com>
2021-01-25 18:56:32 +0100

    Initial commit

---
+++ types.tsx
@@ -0,0 +1,19 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  TabTwo: undefined;
+  TabRanking: undefined;
+  TabProfile: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
